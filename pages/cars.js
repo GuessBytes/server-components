73127edd64761940609cb49b9cf4688e39e1934b
Garvit Mangal <its.me@garvitmangal.com>
2022-11-16 15:47:45 +0530

    upd: hide key

--- pages/cars.js
+++ pages/cars.js
@@ -13,16 +13,6 @@ export default function Cars({ cars }) {
 }
 
 export async function getStaticProps(context) {
-    // const d = await axios.get('https://car-data.p.rapidapi.com/cars', {
-    //     headers: {
-    //         'X-RapidAPI-Key': 'e9e73db6acmsh4d7be4224cd4affp14b87ejsnb8787a16e5ab',
-    //         'X-RapidAPI-Host': 'car-data.p.rapidapi.com'
-    //     },
-    //     params: { limit: 20, page: '0' }
-    // });
-
-    // const { data } = d;
-
     return {
         props: {
             cars: []
